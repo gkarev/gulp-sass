6f2f69e0d6f900be52851bb930df09c82c961f27
German Karev <g.ka.ru@yandex.ru>
2021-01-14 12:12:29 +0300

    добавил сборку html файлов из частей

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ sass.compiler = require('node-sass');
 const browserSync = require('browser-sync').create();
 //const prefix = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const fileinclude = require('gulp-file-include');
 
 const scssPath = './app/style/main.scss';
 const scssAllFilesPath = './app/style/sass/**/*';
 const cssPath = './build/css';
 
+const htmlAllFilesPath = './app/html/**/*';
 
 
 function style() {
@@ -23,19 +25,32 @@ function style() {
 
 }
 
+function html() {
+    return gulp.src(['./app/html/*.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.stream());
+}
+
+
 
 function watch() {
     browserSync.init({
-        port: 3005,
+        port: 3010,
         server: {
-            baseDir: './'
+            baseDir: './build'
         }
     });
+
+    gulp.watch(htmlAllFilesPath, html);
     gulp.watch(scssAllFilesPath, style);
     gulp.watch('./*.html').on('change', browserSync.reload);
 
 }
 
-
+exports.html = html;
 exports.style = style;
 exports.watch = watch;
