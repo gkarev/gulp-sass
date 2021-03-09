4f868232bb3854e105b250a9984b35e731e3658b
German Karev <g.ka.ru@yandex.ru>
2021-03-09 03:19:22 +0300

    ОБНОВЛЕНИЕ СБОРКИ 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,58 @@
-const gulp = require('gulp');
+const { src, dest, series, watch } = require('gulp');
+const del = require('del');
 const sass = require('gulp-sass');
-sass.compiler = require('node-sass');
-const browserSync = require('browser-sync').create();
-//const prefix = require('gulp-autoprefixer');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
-const fileinclude = require('gulp-file-include');
+const include = require('gulp-file-include');
+const sync = require('browser-sync').create();
 
-const scssPath = './app/style/main.scss';
-const scssAllFilesPath = './app/style/sass/**/*';
-const cssPath = './build/css';
-
-const htmlAllFilesPath = './app/html/**/*';
-
-
-function style() {
-    return gulp.src(scssPath)
-        .pipe(sass())
-        // .pipe(prefix('last 2 versions'))
-        // .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(cleanCSS({level: 2}))
-        .pipe(gulp.dest(cssPath))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(browserSync.stream());
-
-}
 
 function html() {
-    return gulp.src(['./app/html/*.html'])
-    .pipe(fileinclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
-    .pipe(gulp.dest('./build'))
-    .pipe(browserSync.stream());
+    return src(['./app/html/**.html'])
+        .pipe(include())
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('./build'))
 }
 
+function scss() {
+    return src('app/scss/main.scss')
+        .pipe(sass())
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(dest('build/css'))
+        .pipe(sass().on('error', sass.logError))
+};
+
+function js() {
+    return src('app/js/main.js')
+        .pipe(include({
+            prefix: '_'
+        }))
+        .pipe(uglify())
+        .pipe(dest('build/js'))
+};
+
+function img() {
+    return src('app/img/**/*')
+        .pipe(imagemin({
+            interlaced: false,
+            progressive: false,
+            optimizationLevel: 3,
+            svgoPlugins: [
+                { removeViewBox: false }
+            ]
+        }))
+        .pipe(dest('build/img'))
+};
 
 
-function watch() {
-    browserSync.init({
+function clear() {
+    return del('build')
+};
+
+function serve() {
+    sync.init({
         port: 3010,
         reloadOnRestart: true,
         server: {
@@ -47,12 +61,13 @@ function watch() {
         }
     });
 
-    gulp.watch(htmlAllFilesPath, html);
-    gulp.watch(scssAllFilesPath, style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+    watch('app/html/**.html', series(html)).on('change', sync.reload)
+    watch('app/scss/**/*.scss', series(scss)).on('change', sync.reload)
+    watch('app/js/**/*.js', series(js)).on('change', sync.reload)
+    watch('app/img/**/*', series(img)).on('change', sync.reload)
+};
 
-}
 
-exports.html = html;
-exports.style = style;
-exports.watch = watch;
+exports.build = series(clear, scss, js, img, html)
+exports.watch = series(clear, scss, js, img, html, serve)
+exports.clear = clear;
